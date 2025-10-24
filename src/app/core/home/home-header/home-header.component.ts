8956e682562a6a924357361bb6cc03389b6937bd
Shane Freed <shane.freed@gmail.com>
2025-10-24 16:19:56 -0400

    minor visual fixes - bouncing screen

--- src/app/core/home/home-header/home-header.component.ts
+++ src/app/core/home/home-header/home-header.component.ts
@@ -9,17 +9,9 @@ import { UtilsService } from 'src/app/theme/shared/services/utils.service';
   styleUrls: ['./home-header.component.scss']
 })
 export class HomeHeaderComponent {
-  public sticky: boolean = false;
+  public sticky: boolean = true;
   public impactDisciplesInfo = impactDisciplesInfo;
 
   constructor(public cartService: CartService, public utilsService: UtilsService,) { }
 
-  // sticky nav
-  @HostListener('window:scroll', ['$event']) onscroll() {
-    if (window.scrollY > 80) {
-      this.sticky = true;
-    } else {
-      this.sticky = false;
-    }
-  }
 }
